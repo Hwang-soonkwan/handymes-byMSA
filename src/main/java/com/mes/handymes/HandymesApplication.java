50e35995abeeab99b2d666d270709a3f07b2773e
Hwang Soonkwan <oksk@soonsoft.co.kr>
2022-09-17 13:54:21 +0900

    Initial commit

---
+++ src/main/java/com/mes/handymes/HandymesApplication.java
@@ -0,0 +1,13 @@
+package com.mes.handymes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandymesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandymesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mes/handymes/HandymesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mes.handymes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandymesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
